82ce117a59a5c7caf9212a384298f7783d87f85b
dnrjs <93022330+dnrjs@users.noreply.github.com>
2021-11-28 02:31:18 +0900

    city

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const navbarIcon = document.querySelector('.navbar__icon');
 const navbarMenu = document.querySelector('.navbar__menu');
 const navbarRightHost = document.querySelector('.navbar__rightmenu__host');
 const navbarRightGlobe = document.querySelector('.navbar__rightmenu__globe');
-const homeSearch = document.querySelector('.home__search');
+const homeSearchChange = document.querySelector('.home__searchchange');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight){
@@ -14,11 +14,27 @@ document.addEventListener('scroll', () => {
         navbarMenu.classList.add('navbarMenu--none');
         navbarRightHost.classList.add('navbarRightHost--black');
         navbarRightGlobe.classList.add('navbarRightGlobe--black');
+        homeSearchShow();
     } else{
         navbar.classList.remove('navbar--dark');
         navbarIcon.classList.remove('navbarIcon--red');
         navbarMenu.classList.remove('navbarMenu--none');
         navbarRightHost.classList.remove('nabar--rightmenu--host');
         navbarRightGlobe.classList.remove('navbar--rightmenu--globe');
+        homeSearchNotShow();
     }
 });
+
+function homeSearchShow() {
+    homeSearchChange.style.visibility = 'visible';
+}
+
+function homeSearchNotShow(){
+    homeSearchChange.style.visibility = 'hidden';
+}
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+

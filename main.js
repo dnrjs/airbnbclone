f7365d3f55dc510a0202757bb9399361adcf96d8
dnrjs <93022330+dnrjs@users.noreply.github.com>
2021-11-30 04:34:35 +0900

    media query

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const navbarIcon = document.querySelector('.navbar__icon');
 const navbarMenu = document.querySelector('.navbar__menu');
 const navbarRightHost = document.querySelector('.navbar__rightmenu__host');
 const navbarRightGlobe = document.querySelector('.navbar__rightmenu__globe');
+const navbarRightToggle = document.querySelector('.navbar__rightmenu__toggle');
 const homeSearchChange = document.querySelector('.home__searchchange');
+const smallSearch = document.querySelector('.smallsearch');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight){
@@ -14,16 +16,28 @@ document.addEventListener('scroll', () => {
         navbarMenu.classList.add('navbarMenu--none');
         navbarRightHost.classList.add('navbarRightHost--black');
         navbarRightGlobe.classList.add('navbarRightGlobe--black');
+        smallSearch.classList.add('smallSearch--white');
         homeSearchShow();
     } else{
         navbar.classList.remove('navbar--dark');
         navbarIcon.classList.remove('navbarIcon--red');
         navbarMenu.classList.remove('navbarMenu--none');
-        navbarRightHost.classList.remove('nabar--rightmenu--host');
-        navbarRightGlobe.classList.remove('navbar--rightmenu--globe');
+        navbarRightHost.classList.remove('navbarRightHost--black');
+        navbarRightGlobe.classList.remove('navbarRightGlobe--black');
+        smallSearch.classList.remove('smallSearch--white');
         homeSearchNotShow();
     }
 });
+
+const scrollUp = document.querySelector('.scrollup');
+window.onmousewheel = function(e){
+    if(e.wheelDelta > 0){
+        scrollUp.classList.add('scrollup--up');
+    }
+    else{
+        scrollUp.classList.remove('scrollup--up');
+    }
+}
 
 function homeSearchShow() {
     homeSearchChange.style.visibility = 'visible';
@@ -32,6 +46,18 @@ function homeSearchShow() {
 function homeSearchNotShow(){
     homeSearchChange.style.visibility = 'hidden';
 }
+
+navbarIcon.addEventListener('click', () => {
+    scrollIntoView('.notice');
+})
+
+
+const navbarRogin = document.querySelector('.navbar__rogin');
+navbarRightToggle.addEventListener('click', () => {
+    navbarRogin.classList.toggle('open');
+})
+
+
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
